7b4763c27bb7ae511f237dbfc9e884d2666d679b
Matt Parisien <63109316+mattparisien@users.noreply.github.com>
2025-07-04 18:23:30 -0400

    S

--- frontend/src/components/Editor/Main.tsx
+++ frontend/src/components/Editor/Main.tsx
@@ -2,7 +2,7 @@
 import FabricEditor from "./FabricEditor/index";
 
 const EditorMain = () => {
-    return <main className="flex w-full h-screen pl-20 pt-20 overflow-hidden">
+    return <main className="flex w-full h-screen pl-20 pt-20 overflow-hidden relative items-center justify-center">
         <FabricEditor width={1080} height={1080} />
     </main>
 }
