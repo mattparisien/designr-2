510ed6cfccdbf73e13e0fe54d59c31d6805e2fa3
Matt Parisien <63109316+mattparisien@users.noreply.github.com>
2025-07-15 18:48:51 -0400

    S

--- frontend/src/app/editor/[id]/components/Toolbar/ToolbarButton.tsx
+++ frontend/src/app/editor/[id]/components/Toolbar/ToolbarButton.tsx
@@ -15,14 +15,12 @@ interface ToolbarButtonProps {
 }
 
 const ToolbarButton = forwardRef<HTMLButtonElement, ToolbarButtonProps>(
-    ({ onClick, isActive, children, className = "", title, rounded = 'xl', icon: Icon, direction = "row" }, ref) => {
+    ({ onClick, isActive, children, className = "", title, icon: Icon, direction = "row" }, ref) => {
         const BUTTON_BASE_CLASSES = "flex text-gray-500 transition cursor-pointer"
         const BUTTON_ICON_CLASSES = "p-1.5"
         const BUTTON_ACTIVE_CLASSES = "bg-gray-100 [&>*]:stroke-[var(--color-accent-90)]"
-        const BUTTON_ROUNDED_XL = "rounded-lg"
-        const BUTTON_ROUNDED_LG = "rounded-lg"
 
-        const roundedClass = rounded === 'xl' ? BUTTON_ROUNDED_XL : BUTTON_ROUNDED_LG
+        const roundedClass = "rounded-[var(--elevated-surface-radius)]";
 
         return (
             <button
